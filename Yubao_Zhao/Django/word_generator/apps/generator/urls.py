6cd230d8ad69c0a356468a4a8074e1c8a2828ac4
Yubao Zhao <zhaoyubao1981@gmail.com>
2016-10-12 15:00:48 -0700

    Refactor Word Generator

--- Yubao_Zhao/Django/word_generator/apps/generator/urls.py
+++ Yubao_Zhao/Django/word_generator/apps/generator/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^generate$', views.generate),
     url(r'^reset$', views.reset)
 ]
--- Yubao_Zhao/Django/word_generator/apps/generator/views.py
+++ Yubao_Zhao/Django/word_generator/apps/generator/views.py
@@ -5,17 +5,13 @@ from string import ascii_uppercase, digits
 def index(request):
     if 'count' not in request.session:
         request.session['count'] = 0
-    if 'word' not in request.session:
-        request.session['word'] = ""
-    return render(request, 'generator/index.html')
-
-def generate(request):
-    if request.method == 'POST':
+        context = {'word' : ''}
+    else:
         request.session['count'] += 1
-        request.session['word'] = ''.join(sample(ascii_uppercase+digits, 14))
-    return redirect('/')
+        rand_word = ''.join(sample(ascii_uppercase+digits, 14))
+        context = {'word' : rand_word}
+    return render(request, 'generator/index.html', context)
 
 def reset(request):
     del request.session['count']
-    del request.session['word']
     return redirect('/')
