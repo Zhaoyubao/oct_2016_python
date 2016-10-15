392bd379c97b52ea167aa83bcf255d79298590cd
Yubao Zhao <zhaoyubao1981@gmail.com>
2016-10-15 10:20:01 -0700

    Update Disappearing Ninjas

--- Yubao_Zhao/Django/disappearing_ninjas/apps/ninjas/views.py
+++ Yubao_Zhao/Django/disappearing_ninjas/apps/ninjas/views.py
@@ -6,13 +6,14 @@ def index(request):
     return render(request, 'ninjas/index.html')
 
 def show_ninja(request, color):
-    ninja = {}
+    ninja = []
     if color:
         if color in turtles:
-            ninja[color] = turtles[color]
+            ninja.append(turtles[color])
         else:
-            ninja['megan'] = 'notapril.jpg'
-        context = {'ninjas' : ninja}
+            ninja.append('notapril.jpg')
     else:
-        context = {'ninjas' : turtles}
+        for val in turtles.values():
+            ninja.append(val)
+    context = {'ninjas' : ninja}
     return render(request, 'ninjas/ninjas.html', context)
